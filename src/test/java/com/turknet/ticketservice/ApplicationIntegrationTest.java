595b99e5f17700e5f2ea64e0acc3f4e68fa9e452
bilal.islam <bilal.islam@turk.net>
2025-03-09 18:21:50 +0300

    first commit

---
+++ src/test/java/com/turknet/ticketservice/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.turknet.ticketservice;
+
+import com.turknet.ticketservice.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
